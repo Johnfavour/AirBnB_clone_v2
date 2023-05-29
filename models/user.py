2d28aa47df81f13891f4fdfe27c1023307bc1fdb
Johnfavour <favourjohn851@gmail.com>
2023-05-29 11:46:58 -0700

    error corrections

--- models/user.py
+++ models/user.py
@@ -4,8 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
-s = "HBNB_TYPE_STORAGE"
-if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+
     class User(BaseModel, Base):
         """This is the class for user
         Attributes:
@@ -15,23 +14,10 @@ if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
         last_name: last name
         """
         __tablename__ = "users"
+
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class User(BaseModel):
-        """This is the class for user
-        Attributes:
-        """
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user" cascade="delete")
+        reviews = relationship("Review", backref="user" cascade="delete")
